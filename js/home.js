277c9d00f993d20a359e421f3de4bec8201e8508
Chamak Saha <chamaksaha58@gmail.com>
2024-09-23 22:20:15 +0600

    added add money with available balance

--- js/home.js
+++ js/home.js
@@ -3,7 +3,10 @@
  s-1: add event handler
     prevent page reload to the account balance
  s-2: get money and pin to be added to the account
-
+ s-3: verify pin number
+ s-4: get the current balance
+ s-5: add inputAddMoney with balance
+ s-6: update the balance in the UI/DOM
 
  */
 
@@ -20,5 +23,31 @@ document.getElementById('btn-send-now').addEventListener('click', function(event
 //  get the pin number provided
     const inputPinNumber = document.getElementById('input-pin-number').value;
     console.log(inputPinNumber);
+
+//  verify pin number
+    if(inputPinNumber === '1234'){
+        console.log('adding money to your account')
+
+        //step-4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step-5: add inputAddMoney with balance
+        const addMoneyNumber = parseFloat(inputAddMoney);
+        //console.log(typeof addMoneyNumber);
+        const balanceNumber = parseFloat(balance);
+        //console.log(typeof balanceNumber);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+        
+        //step-6: update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+
+    }
+    else{
+        alert('Failed to add money!')
+    }
+
+
 
 })
